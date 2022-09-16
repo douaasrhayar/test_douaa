1a2ec3e87952ee07e4684715ba97dbcadeac1661
group9 <group9@gmail.com>
2022-09-16 16:17:37 +0100

    struct

---
+++ douaa pro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	int s , i;
+	s = 0;
+	for(i==1;i<=20;i++){
+		s= s + i;
+	}
+		printf("la somme des 20 premiers entires positifs est:%d",s);
+
+	return 0;
+}
